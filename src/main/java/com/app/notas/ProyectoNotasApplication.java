ca960c25adbee95795aff7f71063fd60c0d0d00a
Renato <renatomartin09@gmail.com>
2025-01-24 19:11:16 -0500

    creacion de las entidades para la relacion con la bd

---
+++ src/main/java/com/app/notas/ProyectoNotasApplication.java
@@ -0,0 +1,13 @@
+package com.app.notas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoNotasApplication.class, args);
+	}
+
+}
